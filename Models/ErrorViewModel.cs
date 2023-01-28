c9606bbd75530c298094cdd6b4f4270d620e2f31
Ignacio Cruz <ignaciojcruz@hotmail.com>
2023-01-28 11:35:14 -0300

    First commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProyectoPortafolioNetCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
